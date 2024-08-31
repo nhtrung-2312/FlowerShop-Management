168096440943a007a4cbad2d747fd67d1807aa43
nhtrung <nhtrung2312@gmail.com>
2024-08-31 21:18:54 +0700

    adding crud for flowers

--- Controllers/FlowerController.cs
+++ Controllers/FlowerController.cs
@@ -1,6 +1,7 @@
 ﻿using FlowerShop_Management.Data;
 using FlowerShop_Management.Models.Flowers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlowerShop_Management.Controllers
 {
@@ -11,9 +12,11 @@ namespace FlowerShop_Management.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var flowers = await _context.Flowers.ToListAsync();
+
+            return View(flowers);
         }
         [HttpGet]
         public IActionResult Add()
@@ -21,9 +24,36 @@ namespace FlowerShop_Management.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Add(AddFlowerViewModel model)
+        public async Task<IActionResult> Add(Flower model)
         {
-            return View();
+            model.Flower_ID = FlowerIDGenerated();
+
+            await _context.Flowers.AddAsync(model);
+            await _context.SaveChangesAsync();
+
+            return Redirect("./Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(string ID)
+        {
+            var flower = _context.Flowers.Where(f => f.Flower_ID == ID);
+            if(flower == null)
+            {
+                return Redirect("./Index");
+            }
+            return View(flower);
+        }
+        public string FlowerIDGenerated()
+        {
+            Random rd = new Random();
+            string id;
+            do
+            {
+                int number = rd.Next(1, 99999);
+                id = $"F{number:D5}";
+            } while (_context.Flowers.Any(f => f.Flower_ID == id));
+
+            return id;
         }
     }
 }
--- Models/Flowers/Flower.cs
+++ Models/Flowers/Flower.cs
@@ -1,10 +1,12 @@
-﻿namespace FlowerShop_Management.Models.Flowers
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlowerShop_Management.Models.Flowers
 {
     public class Flower
     {
-        public Guid ID { get; set; }
+        [Key]
+        public string Flower_ID { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public string Image_url { get; set; }
